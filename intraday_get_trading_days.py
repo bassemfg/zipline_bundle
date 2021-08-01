d7c13795758318a0f8a36617fc53ad70ad595c25
Bassem Yacoube <bassemgeorgi@gmail.com>
2021-08-01 09:43:02 -0700

    .

--- intraday_get_trading_days.py
+++ intraday_get_trading_days.py
@@ -12,11 +12,11 @@ from zipline.utils.calendars import TradingCalendar
 from zipline.utils.tradingcalendar import get_trading_days
 from pandas import Timestamp
 
-#Jul 02 2018 08:00:00 1530518400000000000 TQQQ21725
+#Jul 02 2018 08:00:00 1530518400000000000 TQQQ21725 1627084795000000000 Jul 23 2021 23:59:55 
 #%%
 #This is the process to get the correct trading days, which will then be use in the Bundle Ingest process to index the days
-trading_days = get_trading_days(start=Timestamp('2005-01-03 00:00:00+0000', tz='UTC'), 
-                                end=Timestamp('2020-10-26 02:21:29.456875+0000')
+trading_days = get_trading_days(start=Timestamp('2018-07-02 08:00:00+0000', tz='UTC'), 
+                                end=Timestamp('2021-07-23 23:59:55+0000') #, tz='UTC'
                                 ).date.astype(str)
 
 # %%
@@ -27,7 +27,7 @@ trading_days[trading_days == '2018-12-05']
 
 #%%
 # Create a CSV file to set the Zipline Bundle Ingest process against
-with open('trading_calendar.csv', 'w', newline='') as f:
+with open('intraday_trading_calendar.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(trading_days)
 # %%
