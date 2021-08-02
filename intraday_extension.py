a91ee9fdc07d91159fb103cc82ae4335d6197b99
Bassem Yacoube <bassemgeorgi@gmail.com>
2021-08-01 17:55:23 -0700

    .

--- intraday_extension.py
+++ intraday_extension.py
@@ -7,9 +7,20 @@ from zipline.data.bundles import register
 from zipline.data.bundles.csvdir import csvdir_equities
 
 # Set the start and end dates of the bars, should also align with the Trading Calendar
-start_session = pd.Timestamp('2018-07-02', tz='utc')
-end_session = pd.Timestamp('2021-07-23', tz='utc')
+start_session = pd.Timestamp('2018-07-02', tz='utc') # 08:00:00
+end_session = pd.Timestamp('2021-07-23', tz='utc') # 23:59:00
 
+
+register(
+    'custom-bundle',   # What to call the new bundle
+    csvdir_equities(
+        ['daily'],  # Are these daily or minute bars
+        'C:\\Users\\basse\.zipline\data\\csvs',  # Directory where the formatted bar data is
+    ),
+    calendar_name='NYSE', # US equities default
+    start_session=pd.Timestamp('2005-1-3', tz='utc'),
+    end_session=pd.Timestamp('2020-10-26', tz='utc')
+)
 
 register(
     'intraday-tqqq',   # What to call the new bundle
@@ -23,8 +34,29 @@ register(
     end_session=end_session
 )
 
-
 """
+
+import pandas as pd
+from zipline.data.bundles import register
+from zipline.data.bundles.csvdir import csvdir_equities
+
+# Set the start and end dates of the bars, should also align with the Trading Calendar
+start_session = pd.Timestamp('2005-1-3', tz='utc')
+end_session = pd.Timestamp('2020-10-26', tz='utc')
+
+
+register(
+    'custom-bundle',   # What to call the new bundle
+    csvdir_equities(
+        ['daily'],  # Are these daily or minute bars
+        'C:\\Users\\basse\.zipline\data\\csvs',  # Directory where the formatted bar data is
+    ),
+    calendar_name='NYSE', # US equities default
+    start_session=start_session,
+    end_session=end_session
+)
+
+
 Some commandline reference code on ingesting and cleaning up data bundles
 zipline bundles
 zipline clean -b custom-csvdir-bundle --keep-last 1
